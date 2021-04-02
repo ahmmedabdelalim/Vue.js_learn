58017328849909e85789458675bde8e626a478bc
ahmmedabdelalim <am909962@gmail.com>
2021-04-02 16:40:49 +0200

    seconde commit

---
+++ src/components/Blog/BlogPost.vue
@@ -0,0 +1,83 @@
+<template>
+    <div class="post_box">
+        <span class="post_views"> {{views}}</span>
+        <h3 class="post_title"> {{title}} </h3>
+        <span class="post_date"> {{date}} </span>
+        <p class="post_content"> {{content}} </p>
+
+        <div class="row">
+
+            <div class="col-sm-6"> 
+                <span class="post_author">  
+                    {{auther}} 
+                </span>
+            </div>
+            <div class="col-sm-6 text-right">
+                <span class="post_category tex">{{category}}  </span>
+            </div>
+        </div>
+
+    </div>
+</template>
+
+
+<script lang="ts">
+// @ is an alias to /src
+export default {
+     props:["views","title","date","content","auther","category"],
+  name: "BlogPost",
+ 
+  
+};
+</script>
+<style lang="scss" scoped>
+.post_box{
+    padding: 15px;
+    background-color: #fff;
+    box-shadow: 0 0 10px #eee;
+    position: relative;
+    text-align: left;
+
+    .post_views {
+        position: absolute;
+        top: 5px;
+        right: 5px;
+        background-color: #4caf50;
+        color: #fff;
+        text-align: center;
+        border-radius: 4px;
+        font-weight: bold;
+        padding: 1px 5px ;
+    }
+
+    .post_date {
+        display: block;
+        padding: 0 0 5px;
+        color: #888;
+        font-size: 14px;
+
+    }
+
+    .post_content{
+        line-height: 1.8;
+        color: #666;
+
+    }
+    .post_author {
+        background-color: #607d8b;
+        padding: 6px 10px;
+        border-radius:4px ;
+        color: #fff ;
+
+    }
+
+    .post_category {
+        background-color: #607d8b;
+        padding: 6px 10px;
+        border-radius:4px ;
+        color: #fff ;
+
+    }
+}
+
+</style>
--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -2,29 +2,48 @@
   <div class="about">
     <h1> {{pagename}} </h1>
     <p>{{pagedesc}}</p>
-    <div class="alert alert-warning alert-dismissible fade show" role="alert">
-  <strong>Holy guacamole!</strong> You should check in on some of those fields below.
-  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-    <span aria-hidden="true">&times;</span>
-  </button>
-</div>
-    <Welcome/>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-8">
+          <div class="posts-area">
+            <BlogPost v-for="post in posts" :key="post.id" 
+            :views="post.views"
+            :title="post.date"
+             :auther="post.auther"
+              :content="post.content"
+               :category="post.category"
+            />
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="sidebar"></div>
+        </div>
+      </div>
+    </div>
+     
+   
   </div>
 </template>
 
 
 <script lang="ts">
 // @ is an alias to /src
- 
+ import BlogPost from "@/components/Blog/BlogPost.vue";
+ import postsjson from "../json/MOCK_DATA.json";
 
 export default {  
-  name: "Blog",
+ 
   data:function ()
   {
     return {
       pagename:"the page Blog ",
       pagedesc : "Welcome to Bloge page",
+      posts: postsjson,
     };
-  }
+  },
+   name: "Blog",
+  components: {
+    BlogPost,
+    },
 };
 </script>
